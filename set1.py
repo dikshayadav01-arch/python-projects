3648d6886d1adf94be1a3c153cc8c977e475a9b0
Diksha Yadav <dyadavofficial01@gmail.com>
2026-05-06 11:11:29 +0530

    selenium set 1
    uses chrome driver

---
+++ set1.py
@@ -0,0 +1,20 @@
+from selenium import webdriver
+import time
+
+driver=webdriver.Chrome(executable_path='C:\Users\Diksha\Downloads\chromedriver-win64\chromedriver-win64\chromedriver.exe')
+driver.get('http://www.google.com')
+driver.maximize_window()
+
+input=driver.find_element_by_name('q')
+input.send_keys('selenium')
+time.sleep(5)
+
+button=driver.find_element_by_name('btnX')
+button.click()
+
+time.sleep(5)
+driver.back()
+time.sleep(5)
+driver.forward()
+time.sleep(5)
+driver.quit()
